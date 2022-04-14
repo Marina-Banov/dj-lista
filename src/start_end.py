d6413d74fcf6c44554d2f79c7314515f5aea8d22
mbanov <m.banov7@gmail.com>
2022-04-14 21:27:50 +0200

    add distribution chart

--- src/start_end.py
+++ src/start_end.py
@@ -2,14 +2,18 @@ import sys
 from main import get_db
 
 
+def get_start_end(playlist, id_dict):
+    return id_dict[int(playlist[0])], id_dict[int(playlist[-1])]
+
+
 def main(out_filename="res/res.txt", in_filename="data/data.txt"):
     with open(out_filename) as f:
         line = f.readline().strip().split(',')
-        a, b = line[0], line[-1]
     id_dict = {}
     get_db(in_filename, [], id_dict=id_dict)
-    print(id_dict[int(a)])
-    print(id_dict[int(b)])
+    start, end = get_start_end(line, id_dict)
+    print(start)
+    print(end)
 
 
 if __name__ == "__main__":
